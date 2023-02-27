c4d92a52136a91ce570b276c7a39435a62680465
lixin <lixin@forchange.tech>
2023-02-27 15:14:30 +0800

    feat: 添加用户头像获取的功能

--- src/utils.ts
+++ src/utils.ts
@@ -22,6 +22,8 @@ export const getChatObject = () => {
   const elements = document.querySelectorAll("[class*='min-h-[20px]']");
   if (elements.length <= 0) return;
 
+  const questionnerAvatar = getQuestionerAvatarUrl();
+
   const defaultJson = {
     $schema: "Dialog",
     Dialog: {
@@ -29,7 +31,9 @@ export const getChatObject = () => {
       characters: [
         {
           name: "questioner",
-          avatar: "https://res.pandateacher.com/7KJKIMIE1677146652086.png",
+          avatar:
+            questionnerAvatar ||
+            "https://res.pandateacher.com/1O2BGDK91671292227563.jpg",
         },
         {
           name: "AI",
@@ -87,4 +91,16 @@ export const getChatObject = () => {
   contents = [...contents, ...endContents];
   defaultJson.Dialog.contents = contents;
   return defaultJson;
+};
+
+export const getQuestionerAvatarUrl = () => {
+  let avatarUrl = Array.from(
+    document.querySelectorAll("[class*='w-[30px]']")[0]?.querySelectorAll("img")
+  ).find((item) => item.currentSrc.startsWith("https"))?.currentSrc;
+
+  if (/\?url=/.test(avatarUrl)) {
+    const url = avatarUrl.split("url=")[1];
+    avatarUrl && (avatarUrl = decodeURIComponent(url));
+  }
+  return avatarUrl;
 };
